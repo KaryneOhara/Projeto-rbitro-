146a90005a8909137f290252c0d569b18d5df8f8
KaryneOhara <150279762+KaryneOhara@users.noreply.github.com>
2025-03-06 23:29:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-
-from flask import Flask, redirect, render_template, url_for, request, flash
+from flask import Flask, redirect, render_template, url_for, request, flash, session
 from werkzeug.security import check_password_hash
-from models import User
+from models.arbitro import Arbitro
+from models.contratante import Contratante
 from flask_login import LoginManager, login_user, login_required, logout_user
 import mysql.connector
 
@@ -13,9 +13,14 @@ login_manager.init_app(app)
 
 app.config['SECRET_KEY'] = 'muitodificil'
 
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    arbitro = Arbitro.get(int(user_id))
+    if arbitro:
+        return arbitro
+    contratante = Contratante.get(int(user_id))
+    return contratante
 
 
 #primeira pagina (de abertura)
@@ -29,16 +34,26 @@ def inicial():
 def login():
     if request.method == 'POST':
         email = request.form['email']
-        senha = request.form['senha']   
-        user = User.get_by_email(email)
+        senha = request.form['senha']
 
-        if user and check_password_hash(user['usu_senha'], senha):
-            login_user(User.get(user['usu_id']))
+        arbitro = Arbitro.get_by_email(email)
+        contratante = Contratante.get_by_email(email)
+        if arbitro and check_password_hash(arbitro['arb_senha'], senha):
+            # Guarda o tipo do usuário na sessão
+            session['user_tipo'] = "arbitro"
+            login_user(Arbitro.get(arbitro['arb_id']))
+            return redirect(url_for('home'))
+
+        elif contratante and check_password_hash(contratante['con_senha'], senha):
+            # Guarda o tipo do usuário na sessão
+            session['user_tipo'] = "contratante"
+            login_user(Contratante.get(contratante['con_id']))
             return redirect(url_for('home'))
         
         else:
             flash("Email ou senha inválidos")
     return render_template('login.html')
+
 
 
 #Pagina de cadastro
@@ -51,59 +66,82 @@ def cadastro():
         cpf = request.form['cpf']
         telefone = request.form['telefone']
         senha = request.form['senha']
+        conf_senha = request.form['confirmaSenha']
         tipo = request.form['tipo']
 
-        if not User.exists(email):
-            user = User(nome=nome, email=email, cpf=cpf, telefone=telefone, senha=senha, tipo=tipo)
-            user.add_usuario()            
-            # logar o usuário após cadatro
-            login_user(user)
-            return redirect(url_for('login'))
+        if Arbitro.exists(email) or Contratante.exists(email):
+            return "Email já cadastrado!", 400
         
+        elif senha==conf_senha:
+            if tipo == "arbitro":
+                user = Arbitro(nome=nome, email=email, cpf=cpf, telefone=telefone, senha=senha)
+                user.add_arbitro()            
+                # logar o usuário após cadatro
+                login_user(user)
+                return redirect(url_for('login'))
+            
+            else:
+                user = Contratante(nome=nome, email=email, cpf=cpf, telefone=telefone, senha=senha)
+                user.add_contratante()            
+                # logar o usuário após cadatro
+                login_user(user)
+                return redirect(url_for('login'))
+            
         else:
-            flash("Email já cadastrado")
+            return "As senhas não são equivalentes"
 
     return render_template('login.html')
 
 
 #Pagina principal (usuario)
 @app.route('/home', methods=['GET', 'POST'])
-#@login_required lembrar de apagar  
+@login_required 
 def home():
     return render_template('home.html')
 
 
 #Pagina sobre (informações)
 @app.route('/sobre')
-def sobre():
+def sobre(): 
     return render_template('sobre.html')
 
 
 #Pagina de solicitação (Cadastrar partidas)
 @app.route('/solicitacao')
+@login_required
 def solicitacao():
     return render_template('solicitacao.html')
 
 
 #Pagina onde ficará a galeria (fotos das partidas)
 @app.route('/partidas')
+@login_required
 def partidas():
     return render_template('partidas.html')
 
-#Pagina onde sera o chat (mensagens)
-@app.route('/chat')
-def chat():
-    return render_template('mensagem.html')
-
 #Pagina onde ficara a alteração de dados do usurio (editar)
 @app.route('/configuracoes')
+@login_required
 def configuracoes():
     return render_template('configuracao.html')
 
 
 #Pagina onde ficara as notificações do usuario
 @app.route('/notificacoes')
+@login_required
 def notificacoes():
     return render_template('notificacoes.html')
+
+@app.route('/saiba-mais')
+def saiba_mais():
+    return render_template('saiba_mais.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
